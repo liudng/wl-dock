6dd4582a0276c2abf26123d5e868d4045845e5a1
Liu Dong <liudng@hotmail.com>
2026-07-19 23:46:39 +0000

    build: add version support to wl-dock
    Add project version to CMake config, add version compile definition,
    implement --version/-v command line flag, and set application version

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <QApplication>
 #include <QLoggingCategory>
+#include <QTextStream>
+#include <QString>
 
 #include <LayerShellQt/Shell>
 
@@ -7,8 +9,21 @@
 #include "ForeignToplevelManager.h" // for qRegisterMetaType<ToplevelInfo>
 #include "sni/SniTypes.h"             // for qRegisterMetaType<TrayItemInfo>
 
+#ifndef WL_DOCK_VERSION
+#define WL_DOCK_VERSION "unknown"
+#endif
+
 int main(int argc, char *argv[])
 {
+    // --version / -v：在 QApplication 创建之前处理，避免无谓初始化 Wayland
+    for (int i = 1; i < argc; ++i) {
+        const QString arg = QString::fromLocal8Bit(argv[i]);
+        if (arg == QStringLiteral("--version") || arg == QStringLiteral("-v")) {
+            QTextStream(stdout) << QStringLiteral("wl-dock %1").arg(QStringLiteral(WL_DOCK_VERSION)) << Qt::endl;
+            return 0;
+        }
+    }
+
     qRegisterMetaType<ToplevelInfo>("ToplevelInfo");
     qRegisterMetaType<TrayItemInfo>("TrayItemInfo");
 
@@ -19,6 +34,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QApplication::setApplicationName(QStringLiteral("wl-dock"));
     QApplication::setApplicationDisplayName(QStringLiteral("wl-dock"));
+    QApplication::setApplicationVersion(QStringLiteral(WL_DOCK_VERSION));
     // 切换 TTY 时所有 output 会被移除再恢复，禁止最后窗口关闭时退出
     QApplication::setQuitOnLastWindowClosed(false);
 
